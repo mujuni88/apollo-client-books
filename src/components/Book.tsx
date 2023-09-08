e481ebae018c270e583e1cc7a575882d6cfb810f
Joe Buza <joebm08@gmail.com>
2023-09-08 07:19:57 +0000

    feat(components): add book edit functionality

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,8 +1,8 @@
-import React, { useState, FormEvent } from "react";
+import { Reference, gql, useMutation } from "@apollo/client";
+import { PencilIcon, TrashIcon } from "lucide-react";
+import React, { FormEvent, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { PencilIcon, TrashIcon } from "lucide-react";
-import { gql, useMutation } from "@apollo/client";
 import { useToast } from "./ui/use-toast";
 
 const UPDATE_BOOK = gql`
@@ -22,23 +22,32 @@ const DELETE_BOOK = gql`
 
 export const Book: React.FC<Book> = ({ id, title }) => {
   const [edit, setEdit] = useState(false);
-  const [updateBook, { loading, error }] = useMutation(UPDATE_BOOK);
-  const [deleteBook, { loading:deleting, error: dErr }] = useMutation(DELETE_BOOK);
 
+  return edit ? (
+    <BookEditForm id={id} title={title} onFinish={() => setEdit(false)} />
+  ) : (
+    <BookInfo id={id} title={title} onEdit={() => setEdit(true)} />
+  );
+};
+
+const BookEditForm: React.FC<Book & { onFinish: () => void }> = ({
+  id,
+  title,
+  onFinish,
+}) => {
   const { toast } = useToast();
+  const [input, setInput] = useState(title);
+  const [updateBook, { loading, error }] = useMutation(UPDATE_BOOK);
 
-  if (error || dErr) {
+  if (error) {
     toast({
       variant: "destructive",
-      title: "Error",
+      title: "Error updating book",
     });
   }
 
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleUpdate = async (e: FormEvent) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const title = formData.get("title");
     await updateBook({
       variables: {
         id,
@@ -52,56 +61,97 @@ export const Book: React.FC<Book> = ({ id, title }) => {
         },
       },
     });
-    setEdit(false);
+    onFinish();
   };
 
-  const handleDelete = async(e: FormEvent<HTMLFormElement>) => {
+  return (
+    <form
+      className="grid grid-cols-3 gap-2 items-center"
+      onSubmit={handleUpdate}
+    >
+      <Input
+        name="title"
+        disabled={loading}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="col-span-2"
+      />
+      <div className="space-x-2 grid grid-cols-2">
+        <Button type="submit" disabled={loading || !input}>
+          Update
+        </Button>
+        <Button
+          type="button"
+          variant="outline"
+          onClick={() => {
+            onFinish();
+            setInput(title);
+          }}
+          disabled={loading}
+        >
+          Cancel
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+const BookInfo: React.FC<Book & { onEdit: () => void }> = ({
+  id,
+  title,
+  onEdit,
+}) => {
+  const { toast } = useToast();
+  const [deleteBook, { loading, error }] = useMutation(DELETE_BOOK);
+
+  if (error) {
+    toast({
+      variant: "destructive",
+      title: "Error deleting book",
+    });
+  }
+
+  const handleDelete = async (e: FormEvent) => {
     e.preventDefault();
     deleteBook({
       variables: {
-        id
+        id,
       },
       optimisticResponse: {
         deleteBook: true,
-      }
-    })
-  }
+      },
+      update(cache, { data }) {
+        if (!data.deleteBook) return;
+
+        cache.modify({
+          fields: {
+            books(existingBooks: Reference[], { readField }) {
+              console.log("existingBooks", existingBooks);
+              return existingBooks.filter(
+                (bookRef) => readField("id", bookRef) !== id,
+              );
+            },
+          },
+        });
+      },
+    });
+  };
   return (
-      <div>
-        {edit ? (
-        <form className="grid grid-cols-3 gap-2 items-center" onSubmit={handleSubmit}>
-          
-            <Input name="title" disabled={loading} placeholder={title} className='col-span-2'/>
-            <div className="space-x-2 grid grid-cols-2">
-              <Button type="submit" disabled={loading}>
-                Update
-              </Button>
-              <Button
-                variant="outline"
-                onClick={() => setEdit(false)}
-                disabled={loading}
-              >
-                Cancel
-              </Button>
-            </div>
-          </form>
-        ) : (
-          <form className="grid grid-cols-3 gap-2 items-center" onSubmit={handleDelete}>
-            <p className="text-bold col-span-2">{title}</p>
-            <div>
-
-            <Button variant={"secondary"} onClick={() => setEdit(true)} disabled={deleting}>
-              <PencilIcon size={16} className='mr-2'/>
-              Edit
-            </Button>
-            <Button type="submit" variant={"destructive"} disabled={deleting}>
-            <TrashIcon size={16} className='mr-2'/>
-
-                Delete
-              </Button>
-            </div>
-          </form>
-        )}
+    <form
+      className="grid grid-cols-3 gap-2 items-center"
+      onSubmit={handleDelete}
+    >
+      <p className="text-bold col-span-2">{title}</p>
+      <div className="space-x-2 grid grid-cols-2">
+        <Button variant={"secondary"} onClick={onEdit} disabled={loading} type='button'>
+          <PencilIcon size={16} className="mr-2" />
+          Edit
+        </Button>
+        <Button variant={"destructive"} disabled={loading} type="submit">
+          <TrashIcon size={16} className="mr-2" />
+          Delete
+        </Button>
       </div>
+    </form>
   );
 };
