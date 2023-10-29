4ffce092eaa2593db3db7e74553256bdb332a432
Joe Buza <jbuza@netflix.com>
2023-10-29 16:24:09 -0700

    fix build failure

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: `https://qrq7zj-4000.csb.app`,
+  uri: `http://localhost:4000/graphql`,
   cache: new InMemoryCache(),
 });
